f3f5f0f11d63b0f092522e1a9faf2c3c5bdb219d
Tanmoy Das <100254217+Tan12d@users.noreply.github.com>
2023-07-20 00:56:39 +0530

    Add files via upload

---
+++ Seventh/turtle second.py
@@ -0,0 +1,20 @@
+import turtle as t
+import time
+
+t.bgcolor("black")
+t.pencolor("green")
+a=0
+b=0
+t.speed(0)
+t.penup()
+t.goto(0,200)
+t.pendown()
+while True:
+    t.forward(a)
+    t.right(b)
+    a+=3
+    b+=1
+    if b==210:
+        break
+    t.hideturtle()
+time.sleep(10)
